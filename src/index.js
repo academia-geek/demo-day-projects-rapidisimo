2f3ac48323b2936ba49d823b02e6cdacf9dccf3a
dandeville <dandeville26@gmail.com>
2022-05-31 15:52:19 -0500

    Se agregan librerias de estilos, componentes y utilidades

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Base
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Styles
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Pages
+import App from './App'
+
+// Material Ui
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme/Material'
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
+  document.getElementById('root')
+)
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const { themeGeneral } = require('./src/theme/bridge')
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': themeGeneral.primary,
+        'secondary': themeGeneral.secondary,
+        'tertiary': '#b9e4c9',
+        'quaternary': '#0E3FA9',
+        'background': themeGeneral.background,
+        'white': '#FFFFFF',
+        'black': '#000000',
+      },
+    },
+  },
+  plugins: [],
+}
