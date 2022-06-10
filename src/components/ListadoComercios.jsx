b754add9f05dffe06774a415de26df1086d24675
dandeville <dandeville26@gmail.com>
2022-06-10 16:07:42 -0500

    Se consume servicio de comercios

--- src/components/ListadoComercios.jsx
+++ src/components/ListadoComercios.jsx
@@ -1,37 +1,55 @@
 // Base
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-const ComercioPerfil = ({ nameCommerce, logo, nit }) => {
+// Material UI
+import { Avatar } from "@mui/material";
+
+// Material UI Icons
+import StoreMallDirectoryOutlinedIcon from "@mui/icons-material/StoreMallDirectoryOutlined";
+
+const ComercioPerfil = ({ nameCommerce, email }) => {
   return (
     <div
       className="
         relative
-        flex items-center justify-start gap-4
-        min-w-[320px] max-w-[320px] h-24
-        p-4 md:p-5
+        flex items-center justify-start gap-3
+        min-w-[340px] max-w-[340px] h-24
+        p-4 md:px-3 md:py-4
         bg-white rounded-md shadow-lg
         border-l-4 border-secondary
       "
     >
-      <img
-        className="w-14 h-14 object-cover rounded-full"
-        src={logo}
-        alt="Commerce Logo Profile"
-      />
+      <Avatar sx={{ width: 56, height: 56, bgcolor: "#3e7cb1" }}>
+        <StoreMallDirectoryOutlinedIcon sx={{ fontSize: 32 }} />
+      </Avatar>
 
       <section>
-        <h5 className="mb-0 text-lg font-medium text-ellipsis overflow-hidden">
+        <h5 className="mb-1 text-lg font-semibold text-ellipsis overflow-hidden">
           {nameCommerce}
         </h5>
 
-        <p className="mb-0 text-lg text-ellipsis overflow-hidden">NIT: {nit}</p>
+        <p className="mb-0 text-base font-medium text-ellipsis overflow-hidden">
+          Email: <span className="font-light">{email}</span>
+        </p>
       </section>
     </div>
   );
 };
 
 const ListarComercios = () => {
+  const [comercios, setComercios] = useState([]);
+
+  const fetchComercios = async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_HOST}/allCompanies/`);
+    const data = await response.json();
+    setComercios(data);
+  };
+
+  useEffect(() => {
+    fetchComercios();
+  }, []);
+
   return (
     <div className="w-full h-auto mb-6">
       <h4 className="font-medium md:font-normal text-xl md:text-2xl mb-4">
@@ -39,7 +57,14 @@ const ListarComercios = () => {
       </h4>
 
       <section className="scroll-app flex flex-nowrap gap-4 overflow-y-auto">
-        <ComercioPerfil />
+        {comercios.map((comercio) => (
+          <ComercioPerfil
+            key={comercio.id}
+            nameCommerce={comercio.name_company}
+            email={comercio.email_company}
+            {...comercio}
+          />
+        ))}
       </section>
     </div>
   );
@@ -48,13 +73,13 @@ const ListarComercios = () => {
 ComercioPerfil.propTypes = {
   nameCommerce: PropTypes.string,
   logo: PropTypes.string,
-  nit: PropTypes.string,
+  email: PropTypes.string,
 };
 
 ComercioPerfil.defaultProps = {
   nameCommerce: "Comercio",
   logo: "https://picsum.photos/200/300",
-  nit: "000000000000",
+  email: "compañia@compañia.com",
 };
 
 export default ListarComercios;
