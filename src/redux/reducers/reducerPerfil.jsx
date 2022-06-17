be8e1d315b4b6a141fbd28f56b7c7d7bedd5d0a4
dandeville <dandeville26@gmail.com>
2022-06-17 10:54:30 -0500

    Se crean archivos de redux de perfil para el manejo de sesiones

---
+++ src/redux/reducers/reducerPerfil.jsx
@@ -0,0 +1,61 @@
+// Types
+import { typesPerfil } from "../types/typesPerfil"
+
+const initialState = {
+  listaPerfiles: [],
+  perfilActual: {
+    "id_user": null,
+    "email": "",
+    "document": null,
+    "name": "",
+    "lastname": "",
+    "phone": null,
+    "delivery_man_status": "",
+    "vehicle": "",
+    "rol": "Admin",
+    "user_image": ""
+  },
+  loader: false
+}
+
+export const reducerPerfil = (state = initialState, action) => {
+  switch (action.type) {
+    case typesPerfil.estadoPorDefecto:
+      return {
+        ...initialState
+      }
+
+    case typesPerfil.perfilPorDefecto:
+      return {
+        ...state,
+        perfilActual: {
+          ...state.perfilActual,
+        }
+      }
+
+    case typesPerfil.listarPerfiles:
+      return {
+        ...state,
+        listaPerfiles: action.payload
+      }
+
+    case typesPerfil.actualizarPerfil: {
+      return {
+        ...state,
+        perfilActual: {
+          ...state.perfilActual,
+          ...action.payload
+        }
+      }
+    }
+
+    case typesPerfil.actualizarLoader:
+      return {
+        ...state,
+        loader: action.payload
+      }
+
+    default:
+      return state
+  }
+}
